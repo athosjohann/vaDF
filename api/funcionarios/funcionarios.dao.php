33d3cffc8bf02881b405d24812710b8e3a112308
pablosempreceub <73615179+pablosempreceub@users.noreply.github.com>
2020-11-05 11:05:31 -0300

    Update funcionarios.dao.php

--- api/funcionarios/funcionarios.dao.php
+++ api/funcionarios/funcionarios.dao.php
@@ -17,7 +17,7 @@ Class FuncionariosDAO{
                 and senha = :senha";
 
         $retorno = $this->conexao->ExecutarSQL($sql, $parametros);
-        var_dumpr($retorno);
+        var_dump($retorno);
         if(empty($retorno['dados'])){
             return array("sucesso" => false, "mensagem" => "Usuario ou senha invalidos!");
         }
