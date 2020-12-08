63a5743a2d0d523ba4ba1fc9f7b6ed004b5722f4
Pablo <oliveira.pablo.2012@gmail.com>
2020-12-08 19:19:41 -0300

    Ajustes badges

--- api/badges/badges.dao.php
+++ api/badges/badges.dao.php
@@ -10,26 +10,26 @@ Class BadgesDAO{
 
     public function consultar($parametros){
         $sql = "SELECT 
-                tbl_badge.id_badge,
-                tbl_badge.nome_badge,
-                tbl_badge.requisito,
-                tbl_badge.url_imagem,
-                tbl_badge.experiencia,
-                tbl_badge.matricula_cadastro,
+                tbl_badges.id_badge,
+                tbl_badges.nome_badge,
+                tbl_badges.requisito,
+                tbl_badges.url_imagem,
+                tbl_badges.experiencia,
+                tbl_badges.matricula_cadastro,
                 tbl_funcionarios.nome_funcionario
             FROM tbl_badges where 1 = 1";
         if(!empty($parametros['id_badge'])){
-            $sql .= " and tbl_badge.id_badge = :id_badge ";
+            $sql .= " and tbl_badges.id_badge = :id_badge ";
         }
         if(!empty($parametros['nome_badge'])){
             $parametros['nome_badge'] = '%' . str_replace(' ','%', $parametros['nome_badge']) . '%';
-            $sql .= " and upper(tbl_badge.nome_badge) like upper(:nome_badge) ";
+            $sql .= " and upper(tbl_badges.nome_badge) like upper(:nome_badge) ";
         }
         if(!empty($parametros['experiencia'])){
-            $sql .= " and tbl_badge.experiencia = :experiencia";
+            $sql .= " and tbl_badges.experiencia = :experiencia";
         }
         if(!empty($parametros['experienciaini']) && !empty($parametros['experienciafim'])){
-            $sql .= " and tbl_badge.experiencia between :experienciaini and :experienciafim";
+            $sql .= " and tbl_badges.experiencia between :experienciaini and :experienciafim";
         }        
         
         $retorno = $this->conexao->ExecutarSQL($sql, $parametros);
@@ -50,7 +50,7 @@ Class BadgesDAO{
         if(!empty($erros)){
             return $erros;
         }
-        $sql = "INSERT INTO tbl_categorias (
+        $sql = "INSERT INTO tbl_badges (
                             nome_badge,
                             requisito,
                             url_imagem,
