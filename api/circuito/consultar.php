115e46a1eaa2d6e8a954aafb973c284fcefabfa0
Pablo <oliveira.pablo.2012@gmail.com>
2020-12-08 19:44:16 -0300

    Consultar circuitos

--- api/circuito/consultar.php
+++ api/circuito/consultar.php
@@ -1,6 +1,6 @@
 <?php
 require_once("../api-header.php");
-require_once("badges.dao.php");
+require_once("circuitos.dao.php");
 
 $dao = new BadgesDAO($ConexaoBanco);
 $retorno = $dao->consultar($dadosRecebidos);
